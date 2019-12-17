9fcb91bacb39a0c3c285e39f3e68b14ac5e52c00
Pim Snel <pim@lingewoud.nl>
2019-12-17 23:41:06 +0100

    make themable

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App';
 import bridge from './utils/main-process-bridge';
 
-//STYLE STUFF
-import './css/index.css';
-import './css/bootstrap-grid.css';
+import service from './services/service';
+
+service.getConfigurations().then((c) => {
+  let cssIndex = require('./themes/' + c.global.appTheme + '/css/index.css');
+  let cssBootstrap = require('./themes/' + c.global.appTheme + '/css/bootstrap-grid.css');
+})
 
 const isDev = window.require('electron-is-dev');
 if(isDev){
@@ -16,10 +19,9 @@ else{
     console.log('Running in production');
 }
 
-
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
-    document.getElementById('root')
+  <BrowserRouter>
+    <App />
+      </BrowserRouter>,
+  document.getElementById('root')
 );
