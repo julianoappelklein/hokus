ec5f7c0497e5ac8c9bc902ff4574f082260c2e42
Daniel Low <daniellowtw@gmail.com>
2020-03-10 00:47:24 +0000

    Add incremental TS migration support
    This commit demonstrates a POC that we can do gradual typescript
    migration.
    
    Using this tool https://github.com/Khan/flow-to-ts we can mechanically
    convert js(x) files to ts(x) files. However, it produces rather weakly
    typed code and a human is needed to resolve compilation error.
    
    I've tested this migration by building a Windows binary and it seems to
    work fine.

---
+++ global-types.ts
@@ -0,0 +1,87 @@
+
+
+export type ServeConfig = {
+  key: string;
+  config: string;
+};
+
+export type BuildConfig = {
+  key: string;
+  config: string;
+};
+
+export type PublisherConfig<P> = {
+  type: string;
+} & P;
+
+export type SitePublishConfig<P> = {
+  config: PublisherConfig<P>;
+  key: string;
+};
+
+export type SiteSource<S> = {
+  type: string;
+} & S;
+
+
+export type RawSiteConfig = {
+  key: string;
+  name: string;
+  source: SiteSource<any>;
+  // transform: Array<SiteTransformConfig<*>>,
+  publish: Array<SitePublishConfig<any>>;
+};
+
+export type SiteConfig = RawSiteConfig & {
+  configPath: string;
+};
+
+export type WorkspaceConfigRaw = {
+  hugover: string;
+  serve: Array<ServeConfig>;
+  build: Array<BuildConfig>;
+  singles: Array<SingleConfig>;
+  collections: Array<CollectionConfig>;
+};
+
+export type WorkspaceConfig = WorkspaceConfigRaw & {
+  key: string;
+  path: string;
+};
+
+export type WorkspaceHeader = {
+  key: string;
+  path: string;
+  state: "mounted" | "unmounted";
+};
+
+export type SingleConfig = {
+  key: string;
+  title: string;
+  file: string;
+  fields: Array<any>;
+};
+
+export type CollectionConfig = {
+  key: string;
+  title: string;
+  itemtitle: string;
+  fields: Array<any>;
+  folder: string;
+  extension: string;
+  dataformat: string;
+};
+
+export type EmptyConfigurations = {
+  empty: true;
+  fileSearchPatterns: Array<string>;
+};
+
+export type Configurations = {
+  sites: Array<SiteConfig>;
+  global: {
+    siteManagementEnabled: boolean;
+    debugEnabled: boolean;
+    cookbookEnabled: boolean;
+  };
+};
